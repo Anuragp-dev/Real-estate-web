bdc1c29669773f78741e60df1255115860eaaa0f
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2024-10-15 18:37:41 +0530

    added home page

---
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./HomePage.scss"
+
+
+const HomePage = () => {
+    return (
+        <div className='layout'>
+            homePage
+        </div>
+    )
+}
+
+export default HomePage

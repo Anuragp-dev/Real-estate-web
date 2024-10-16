713efaee54ded205f7a138ee0880c538bf19ffa8
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2024-10-16 13:10:13 +0530

    updated search bar

--- frontend/src/components/searchBar/SearchBar.jsx
+++ frontend/src/components/searchBar/SearchBar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const SearchBar = () => {
   return (
-    <div>
-      searchBar
+    <div className="searchBar">
+      <div className="type">
+        <button>Buy</button>
+        <button>Rent</button>
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="Enter City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={100000000}
+          placeholder="Enter Min Price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={100000000}
+          placeholder="Enter Max Price"
+        />
+        <button>
+            <img src="/search.png" alt="" />
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;

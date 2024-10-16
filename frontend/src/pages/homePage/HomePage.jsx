6d4dac2c0f6123f5ac03fedefa6bc2d0c4cb8541
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2024-10-16 11:42:27 +0530

    changes in home page

--- frontend/src/pages/homePage/HomePage.jsx
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import "./HomePage.scss"
+import SearchBar from '../../components/searchBar/SearchBar'
 
 
 const HomePage = () => {
     return (
         <div className='homePage'>
-            <div className='textContainer'>text</div>
-            <div className='imgContainer'>image</div>
+            <div className='textContainer'>
+                <div className="wrapper">
+                    <h1 className="title">
+                        Find Real Estate & Get Your Dream Place
+                    </h1>
+                    <p className=''>
+                        Your trusted destination for finding the perfect home.
+                        Whether you're buying, selling, or renting,
+                        we are here to guide you every step of the way.
+                        Explore a wide range of properties tailored to your needs,
+                        from cozy apartments to luxurious homes in your dream locations.
+                        With our user-friendly platform, personalized service,
+                        and expert advice, you can navigate the real estate market with confidence.
+                        Start your journey to homeownership today!
+                    </p>
+                    <SearchBar />
+                    <div className="box">
+                        <h1>16+</h1>
+                        <h2>Years of Experience</h2>
+                    </div>
+                    <div className="box">
+                        <h1>200+</h1>
+                        <h2>Award gained</h2>
+                    </div>
+                    <div className="box">
+                        <h1>1200+</h1>
+                        <h2>Property Ready</h2>
+                    </div>
+                </div>
+            </div>
+            <div className='imgContainer'>
+                <img src="/bg.png" alt="background" />
+            </div>
         </div>
     )
 }

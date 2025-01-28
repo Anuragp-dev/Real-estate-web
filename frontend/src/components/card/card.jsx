d3830e79950e4ad724a6f6f63186494cb1e84cad
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-01-28 14:52:10 +0530

    changes

--- frontend/src/components/card/card.jsx
+++ frontend/src/components/card/card.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './card.scss'
 
-const Card = () => {
+
+const Card = ({ item }) => {
   return (
-    <div>card</div>
+    <div className='card'>
+      <Link to={`/${item.id}`} className='imageContainer'>
+        <img src={item.img} alt='image' />
+      </Link>
+      <div className='textContainer'>
+        <h2 className='title'>
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className='address'>
+          <img src="/pin.png" alt="image" />
+          <span>{item.address}</span>
+        </p>
+        <p className='price'>${item.price}</p>
+        <div className="bottom">
+          <div className='features'>
+            <div className="feature">
+              <img src="/bed.png" alt="image" />
+              <span>{item.bedroom} Beds</span>
+            </div>
+            <div className="feature">
+              <img src="/bath.png" alt="image" />
+              <span>{item.bathroom} Bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/filter/filter.jsx
+++ frontend/src/components/filter/filter.jsx
@@ -3,8 +3,53 @@ import './filter.scss'
 
 const Filter = () => {
   return (
-    <div>
-      filter
+    <div className='filter'>
+      <h1>Search results for <b>london</b></h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor='city'>Location</label>
+          <input type='text' id='city' placeholder='city location' />
+        </div>
+      </div>
+      <div className="bottom">
+      <div className="item">
+          <label htmlFor='type'>Type</label>
+          <select name="type" id="type">
+            <option value="all">All</option>
+            <option value="buy"> Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+
+        <div className="item">
+          <label htmlFor='property'>Property</label>
+          <select name="property" id="property">
+            <option value="all">All</option>
+            <option value="apartment">Apartment</option>
+            <option value="villa">Villa</option>
+            <option value="house">House</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+
+        <div className="item">
+          <label htmlFor='min price'>Min Price</label>
+          <input type='number' id='min price' placeholder='min price' />
+        </div>
+
+        <div className="item">
+          <label htmlFor='max price'>Max price</label>
+          <input type='number' id='max price' placeholder='max price' />
+        </div>
+
+        <div className="item">
+          <label htmlFor='city'>Bedroom</label>
+          <input type='text' id='bedroom' placeholder='bedroom' />
+        </div>
+        <button>
+          <img src="/search.png" alt="search" />
+        </button>
+      </div>
     </div>
   )
 }

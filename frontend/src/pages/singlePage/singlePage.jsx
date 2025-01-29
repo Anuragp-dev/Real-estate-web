075395b31816e8930228645f4c34f2fc71d9b4d4
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-01-29 12:49:08 +0530

    single page section

--- frontend/src/pages/singlePage/singlePage.jsx
+++ frontend/src/pages/singlePage/singlePage.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import './singlePage.scss'
 import Slider from '../../components/slider/slider'
+import { singlePostData, userData } from '../../lib/dummydata'
 
 const SinglePage = () => {
   return (
     <div className='singlePage'>
       <div className="details">
         <div className="wrapper">
-          <Slider />
+          <Slider images={singlePostData.images} />
           <div className="info">
-            <div className="top"></div>
-            <div className="bottom"></div>
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">
+                  ${singlePostData.price}
+                </div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
           </div>
         </div>
       </div>

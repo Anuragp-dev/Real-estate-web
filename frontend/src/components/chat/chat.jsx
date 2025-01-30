b00079f44d0fcb5ddb269a71a00ec7311067add1
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-01-30 17:10:14 +0530

    chat box

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './chat.scss'
 
 const Chat = () => {
+    const [Chat, setChat] = React.useState(true)
     return (
         <div className='chat'>
             <div className="messages">
@@ -34,8 +35,64 @@ const Chat = () => {
                         Lorem ipsum dolor sit amet consectetur..
                     </p>
                 </div>
+                <div className="message">
+                    <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <span>John Doe</span>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur..
+                    </p>
+                </div>
+                <div className="message">
+                    <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <span>John Doe</span>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur..
+                    </p>
+                </div>
             </div>
-            <div className='chatBox'></div>
+            {Chat && (<div className='chatBox'>
+                <div className="top">
+                    <div className="user">
+                        <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                        John Doe
+                    </div>
+                    <span className='close' onClick={() => setChat(null)}>x</span>
+                </div>
+                <div className="center">
+                    <div className="chatMessage">
+                        <p>leojfjfd.kfjdskfjjdsfkdsjfkdjk</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage own">
+                        <p>leojfjfd.kfjdskfjjdsfkdsjfkdjk</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage ">
+                        <p>leojfjfd.kfjdskfjjdsfkdsjfkdjk</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage own">
+                        <p>leojfjfd.kfjdskfjjdsfkdsjfkdjk</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage">
+                        <p>leojfjfd.kfjdskfjjdsfkdsjfkdjk</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage own">
+                        <p>leojfjfd.kfjdskfjjdsfkdsjfkdjk</p>
+                        <span>1 hour ago</span>
+                    </div>
+                    <div className="chatMessage">
+                        <p>leojfjfd.kfjdskfjjdsfkdsjfkdjk</p>
+                        <span>1 hour ago</span>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <textarea name="" id=""></textarea>
+                    <button>Send</button>
+                </div>
+            </div>)}
         </div>
     )
 }

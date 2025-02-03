f0e3e329b777042d4d05da5eba4af4627586bfdf
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-02-03 16:39:01 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import authRoutes from "./routes/authRoutes.js"
 
 const app = express()
 
@@ -6,7 +7,9 @@ const app = express()
 
 
 
+app.use(express.json())
 
+app.use("/api/auth", authRoutes)
 app.listen(8800, () => {
     console.log("server is running")
 })
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,2 +1,55 @@
+import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js"
+
+export const register = async (req, res) => {
+
+    try {
+        const { username, email, password, avatar } = req.body
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+                avatar
+            }
+        })
 
 
+        res.status(201).json({ message: "User created successfully" })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong" })
+    }
+
+}
+
+// login
+
+const login = async (res, req) => {
+
+    const { email, password } = req.body
+
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" })
+    }
+    // check if user exists
+    const user = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    })
+
+
+    if (!user) {
+        res.status(401).json({ message: "Invalid credentials" })
+
+    }
+}

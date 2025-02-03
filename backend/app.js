a6cc68acdb61ae097d66024ac25a620748d0ae8b
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-02-03 18:08:29 +0530

    added auth routes api

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import authRoutes from "./routes/authRoutes.js"
 
 const app = express()
@@ -6,10 +7,15 @@ const app = express()
 
 
 
-
+app.use(cookieParser())
 app.use(express.json())
 
+// routes
 app.use("/api/auth", authRoutes)
+
+
+
+
 app.listen(8800, () => {
     console.log("server is running")
 })
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 import prisma from "../lib/prisma.js"
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
 
@@ -32,24 +33,64 @@ export const register = async (req, res) => {
 
 // login
 
-const login = async (res, req) => {
+export const login = async (req, res) => {
 
     const { email, password } = req.body
+    // console.log('email: ', email)
 
 
-    if (!email || !password) {
-        return res.status(400).json({ message: "All fields are required" })
-    }
-    // check if user exists
-    const user = await prisma.user.findUnique({
-        where: {
-            email: email
+    try {
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" })
         }
-    })
+        // check if user exists
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
 
 
-    if (!user) {
-        res.status(401).json({ message: "Invalid credentials" })
+        if (!user) {
+            res.status(401).json({ message: "Invalid credentials" })
+
+        }
+
+        // check if password is correct
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordCorrect) {
+            return res.status(401).json({ message: "Invalid Password" })
+        }
+
+
+        const age = 1000 * 60 * 60 * 24 * 7
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+            expiresIn: age
+        })
+
+        res
+            .cookie("token", token, {
+                httpOnly: true,
+                // secure:true
+                maxAge: age
+            })
+            .status(200)
+            .json({ message: "Login successful" })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "faild to login" })
 
     }
+}
+
+// logout
+export const logout = (req, res) => {
+
+    res.clearCookie("token")
+        .status(200)
+        .json({ message: "Logout successful" })
 }

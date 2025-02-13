4bca5b972559fa2f01c9a6235590223db3afc512
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-02-13 11:37:41 +0530

    changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+import prisma from "../lib/prisma"
+
 export const getUsers = async (req, res) => {
 
     try {
@@ -12,3 +14,85 @@ export const getUsers = async (req, res) => {
         res.status(500).json({ message: "Something went wrong" })
     }
 }
+
+
+export const getUser = async (req, res) => {
+
+    const id = req.params.id
+
+    try {
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: id
+            }
+        })
+
+        res.status(200).json(user)
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong" })
+    }
+}   
+
+
+// update user info
+
+export const updateUser = async (req, res) => {
+
+    const id = req.params.id
+
+    const { 
+        username,
+        email,
+        password,
+        avatar
+    } = req.body
+
+    try {
+
+        const user = await prisma.user.update({
+            where: {
+                id: id
+            },
+            data: {
+                username,
+                email,
+                password,
+                avatar
+            }
+        })
+
+        res.status(200).json(user)
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong" })
+    }
+}
+
+// delete user
+
+export const deleteUser = async (req, res) => {
+
+    const id = req.params.id
+
+    try {
+
+        const user = await prisma.user.delete({
+            where: {
+                id: id
+            }
+        })
+
+        res.status(200).json(user)
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong" })
+    }
+}   
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,14 @@
 
 import express from "express";
 import { register, login, logout } from "../controllers/authController.js";
+import { getUser, getUsers } from "../controllers/userController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.get("/getUsers", getUsers);
+router.get("/getUser/:id",verifyToken, getUser);
 router.post("/logout", logout);
 
 export default router;

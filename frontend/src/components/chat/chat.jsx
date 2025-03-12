83539bc75a5ddb27ee353ffd0297c7233ec979a8
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-03-12 12:05:41 +0530

    changes

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import './chat.scss'
 import { AuthContext } from '../../context/AuthContext';
 import apiRequest from '../../lib/apiRequest';
+import { format } from "timeago.js"
 
 const Chat = ({ chats }) => {
     const [Chat, setChat] = React.useState(null)
@@ -15,6 +16,21 @@ const Chat = ({ chats }) => {
             console.log(error)
         }
     }
+
+    const handleSubmit = async (e) => {
+
+        const formData = new FormData(e.target);
+        const text = formData.get("text");
+        try {
+            const res = await apiRequest.post("/messages/" + Chat.id, { text })
+            setChat((prev) => ({ ...prev, messages: [...prev.messages, res.data] }))
+            e.target.reset()
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
 
     return (
         <div className='chat'>
@@ -47,25 +63,35 @@ const Chat = ({ chats }) => {
                     <span className='close' onClick={() => setChat(null)}>x</span>
                 </div>
                 <div className="center">
-                { Chat.messages.map((message) => (
-                    
-                    <div className="chatMessage own" key={message.id}>
-                        <p>{message.text}</p>
-                        <span>1 hour ago</span>
-                    </div>
-                ))
+                    {Chat.messages.map((message) => (
 
-                }
-                    
-                    <div className="chatMessage">
+                        <div className="chatMessage"
+
+                            style={{
+                                alignSelf: message.userId === currentUser.id ? "flex-end" : "flex-start",
+                                textAlign: message.userId === currentUser.id ? "right" : "left"
+                            }}
+
+                            key={message.id}
+                        >
+                            <p>{message.text}</p>
+                            <span>1 hour ago</span>
+                        </div>
+                    ))
+
+                    }
+
+                    <div className="chatMessage"
+
+                    >
                         <p>leojfjfd.kfjdskfjjdsfkdsjfkdjk</p>
-                        <span>1 hour ago</span>
+                        <span>{format(message.createdAt)}</span>
                     </div>
                 </div>
-                <div className="bottom">
-                    <textarea ></textarea>
+                <form onSubmit={handleSubmit} className="bottom">
+                    <textarea name='text' ></textarea>
                     <button>Send</button>
-                </div>
+                </form>
             </div>
             )}
         </div>

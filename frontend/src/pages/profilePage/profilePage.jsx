36a41354e91da3c74d4b55761f7beff08f52e764
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-03-17 18:21:13 +0530

    changes

--- frontend/src/pages/profilePage/profilePage.jsx
+++ frontend/src/pages/profilePage/profilePage.jsx
@@ -85,7 +85,7 @@ const ProfilePage = () => {
                             resolve={data.chatResponse}
                             errorElement={<p>Error loading chats!</p>}
                             >
-                            {(chatResponse) => <Chat chats={chatResponse.data} />}
+                            {(chatResponse) => <Chat chats={chatResponse.data} isMessage={true} />}
                         </Await>
                     </Suspense>
                 </div>
